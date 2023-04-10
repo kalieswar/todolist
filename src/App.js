64c8d6a7e5bcceab91573bd0e73aa9ea34d6e53b
Kalieswar <kaliesbhuvanesh25@gmail.com>
2023-04-10 18:15:21 +0530

    monday commit

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import { useEffect, useState } from 'react';
 import Todo from './components/Todo';
 import { myStore } from './firebase/Config';
-import { collection, onSnapshot, serverTimestamp, addDoc, orderBy, query } from 'firebase/firestore';
+import { collection, onSnapshot, addDoc, orderBy, query } from 'firebase/firestore';
 
 const q = query(collection(myStore, 'todos'), orderBy('timestamp', 'desc'))
 function App() {
   const [todos, setTodos] = useState([])
   const [input, setInput] = useState('');
+  const [date, setDate] = useState();
+  const [timeStatus, setTimeStatus] = useState();
   useEffect(() => {
     onSnapshot(q, (snapshot) => {
       setTodos(snapshot.docs.map(doc => ({
@@ -16,15 +18,22 @@ function App() {
         item: doc.data()
       })))
     })
-  }, [input])
-  
+    let getDate = new Date();
+    let getMonth = getDate.getMonth() + 1;
+    let currentDate = getDate.getDate() + '/' + getMonth + '/' + getDate.getFullYear() + ',' + getDate.getHours() + ':' + getDate.getMinutes()
+    setDate(currentDate)
+  }, [input,timeStatus])
+
+
   const addTodo = (e) => {
     e.preventDefault();
     if (input.length > 0) {
 
       addDoc(collection(myStore, "todos"), {
         todo: input,
-        timestamp: serverTimestamp()
+        timestamp: date,
+        status:'Pending',
+        finish:''
       })
       setInput("")
     }
@@ -32,11 +41,11 @@ function App() {
   return (
     <div className="App">
       <h2>To Do List</h2>
-      <form>
+      <form onSubmit={addTodo}>
         <TextField id='outlined-basic' label="Make Todo" variant='outlined' style={{ margin: "0px 5px" }} size='small' value={input}
           onChange={e => setInput(e.target.value)}
         />
-        <Button variant='contained' color='primary' onClick={addTodo}>Add Todo</Button>
+        <Button type='submit' variant='contained' color='primary'>Add Todo</Button>
       </form>
       <ul>
         {todos.map(item => <Todo key={item.id} arr={item} />)}
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,44 +1,120 @@
-import {Button, List, ListItem, ListItemAvatar, ListItemText, TextField} from '@mui/material';
+import { Button, List, ListItem, ListItemAvatar, ListItemText, TextField } from '@mui/material';
 import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit";
 import { myStore } from '../firebase/Config';
-import { doc,deleteDoc, updateDoc } from 'firebase/firestore';
+import { doc, deleteDoc, updateDoc } from 'firebase/firestore';
 import '../../src/components/todo.css'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { Dropdown } from 'react-bootstrap';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Modal from 'react-bootstrap/Modal';
+import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
+import { confirmAlert } from 'react-confirm-alert';
+import 'react-confirm-alert/src/react-confirm-alert.css';
 
-const Todo = ({arr})=>{
-    const [editing, setEditing ] = useState(false);
-    const [editedTodo, setEditedTodo] = useState(arr.item.todo)
+const Todo = ({ arr }) => {
+    const [editing, setEditing] = useState(false);
+    const [editedTodo, setEditedTodo] = useState(arr.item.todo);
+    const [status, setStatus] = useState(); 
+    const [dat, setDat] = useState()
+    const [show, setShow] = useState(false);
 
-    const handleEdit = async ()=>{
-        try{
-            const todoRef = doc(myStore, "todos",arr.id);
-            await updateDoc(todoRef, { todo: editedTodo});
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+
+    useEffect(() => {
+        let getDate = new Date();
+        let getMonth = getDate.getMonth() + 1;
+        let currentDate =  getDate.getDate() + '/' + getMonth + '/' + getDate.getFullYear() + ',' + getDate.getHours() + ':' + getDate.getMinutes()
+
+
+        setDat(currentDate)
+    },[])
+
+
+    const handleDelete = (id) => {
+        confirmAlert({
+          title: 'Delete the task',
+          message: 'Are you sure you need to delete this task ?',
+          buttons: [
+            {
+              label: 'Yes',
+              style:{color:"white",backgroundColor:"green"},
+              onClick: () => { deleteDoc(doc(myStore, "todos", id)) }
+            },
+            {
+              label: 'No',
+              style:{color:"white", backgroundColor:"red"}
+            }
+          ]
+        });
+      };
+
+    const handleEdit = async () => {
+        try {
+            const todoRef = doc(myStore, "todos", arr.id);
+            await updateDoc(todoRef, { todo: editedTodo });
             setEditing(false);
-        }catch(error){
+        } catch (error) {
             console.error("Error updating: ", error)
         }
     }
-    return(
-        <List className='todo__list container'>
+    const taskEdit = async (newStatus) => {
+        try {
+            const todotask = doc(myStore, "todos", arr.id);
+            await updateDoc(todotask, { status: newStatus, finish: dat });
+            setStatus(newStatus)
+        } catch (error) {
+            console.error("Error Status Updating: ", error)
+        }
+
+    }
+    return (
+        <>
+        <List className={arr.item.status === 'Pending' ? 'todo__list-pending shadow-lg' : 'todo__list-finished shadow-lg'} >
             <ListItem>
-                <ListItemAvatar/>
+                <ListItemAvatar />
                 {editing ? (
                     <>
-                    <TextField value={editedTodo} onChange={(e)=> setEditedTodo(e.target.value)}/>
-                    <Button variant="contained" onClick={handleEdit}>Save</Button>
+                        <TextField value={editedTodo} onChange={(e) => setEditedTodo(e.target.value)} />
+                        <Button variant="contained" onClick={handleEdit}>Save</Button>
                     </>
-                 ) : (
-             <>
-                    <ListItemText primary={arr.item.todo}/>
-                    <Button onClick={()=> setEditing(true)}><EditIcon/></Button>
+                ) : (
+                    <>
+                        <ListItemText primary={arr.item.todo} secondary={"Created at " + arr.item.timestamp}  />
+                        <ListItemText primary=
+                            {arr.item.status === "Finished" ? (arr.item.status) : (
+                                <>
+                                    <DropdownButton title="Todo Status">
+                                        <Dropdown.Item onClick={() => {
+                                            taskEdit("Finished");
+                                        }}>Finished</Dropdown.Item>
+                                    </DropdownButton>
+                                </>
+                            )}
+                            secondary={arr.item.status === "Finished" ? "Finished at: " + arr.item.finish :  arr.item.status} />
+                        {arr.item.status === "Finished" ? (null) : (<Button onClick={() => setEditing(true)}><EditIcon /></Button>)}
                     </>
-                    
-                    )}
-                
+
+                )}
+
             </ListItem>
-            <DeleteIcon color='error' fontSize='large' style={{opacity: 1}} onClick={()=>{deleteDoc(doc(myStore,"todos", arr.id))}}/>
-        </List>
+            <RemoveRedEyeIcon onClick={handleShow} fontSize='large' style={{opacity:1 , cursor:"pointer", paddingRight:"10px", color:"orange"}}/>
+            <DeleteIcon color='error' fontSize='large' style={{ opacity: 1, cursor: "pointer" }} onClick={() => handleDelete(arr.id)} />
+        </List> 
+        <Modal show={show} onHide={handleClose} className='shadow-lg'> 
+        <Modal.Header closeButton>
+          <Modal.Title>Task Name: {arr.item.todo}</Modal.Title>
+            </Modal.Header>
+        <Modal.Body>
+            <h4 className='py-4'>Task Created Date & Time: {arr.item.timestamp}</h4>
+            <h4 className='py-4'>Task Status: {arr.item.status}</h4>
+            <h4 className='py-4'>Task Finished Date & Time: {arr.item.status === "Finished" ? arr.item.finish : "Time will be updated after finishing the task"}</h4>
+        </Modal.Body>
+      </Modal>
+      </>
     )
 }
 
